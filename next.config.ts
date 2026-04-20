9adf89d786240ef259b8a6775f2fc0c9106f4147
phoenixvp <94145215+PHOENIXVP@users.noreply.github.com>
2026-04-20 06:46:15 +0000

    not clicking issue solved

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  allowedDevOrigins: ["*.csb.app", "*.codesandbox.io", "*.preview.csb.app"],
+  // reactCompiler: true,
 };
 
 export default nextConfig;
